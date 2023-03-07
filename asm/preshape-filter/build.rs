19b9506e6169f6249ea8e77cca77faf6831220df
Rod Conan <rconan@gmto.org>
2023-03-06 22:38:24 -0800

    ASM POSITIONNER

--- asm/preshape-filter/build.rs
+++ asm/preshape-filter/build.rs
@@ -1,19 +1,45 @@
-use std::path::PathBuf;
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+};
 
 fn main() {
-    cc::Build::new()
-        .file("sys/ASM_preshapeBesselF.c")
-        .file("sys/ASM_preshapeBesselF_data.c")
-        .compile("asm_preshape_bessel_filter");
-    println!(
-        "cargo:rustc-link-search=native={}",
-        "libasm_preshape_bessel_filter"
-    );
-    println!("cargo:rustc-link-lib=asm_preshape_bessel_filter");
-    println!("cargo:rerun-if-changed=sys/ASM_preshapeBesselF.h");
+    let lib = env::var("CARGO_PKG_NAME").unwrap();
+    let sys = Path::new("sys");
 
-    let bindings = bindgen::builder()
-        .header("sys/ASM_preshapeBesselF.h")
+    let mut cc_builder = cc::Build::new();
+    let mut bindings = bindgen::builder();
+
+    if let Ok(entries) = fs::read_dir(sys) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let file_name = entry.path();
+                if let Some(extension) = file_name.extension() {
+                    match extension.to_str() {
+                        Some("c") => {
+                            cc_builder.file(file_name);
+                        }
+                        Some("h") => {
+                            bindings = bindings.header(
+                                file_name
+                                    .to_str()
+                                    .expect(&format!("{:?} conversion to str failed", file_name)),
+                            );
+                            println!("cargo:rerun-if-changed={:}", file_name.to_str().unwrap());
+                        }
+                        _ => (),
+                    }
+                }
+            }
+        }
+    }
+
+    cc_builder.compile(lib.as_str());
+
+    println!("cargo:rustc-link-search=native=lib{}", lib);
+    println!("cargo:rustc-link-lib={}", lib);
+
+    let bindings = bindings
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
--- asm/preshape-filter/src/lib.rs
+++ asm/preshape-filter/src/lib.rs
@@ -3,68 +3,8 @@
 #![allow(non_snake_case)]
 #![allow(improper_ctypes)]
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-/// Simulink controller wrapper
-#[derive(Debug, Clone, Copy, Default)]
-pub struct AsmPreshapeFilter {
-    /// Inputs Simulink structure
-    pub inputs: ExtU_ASM_preshapeBesselF_T,
-    /// Outputs Simulink structure
-    pub outputs: ExtY_ASM_preshapeBesselF_T,
-    states: DW_ASM_preshapeBesselF_T,
-}
-impl Default for ExtU_ASM_preshapeBesselF_T {
-    fn default() -> Self {
-        ExtU_ASM_preshapeBesselF_T { AO_cmd: 0f64 }
-    }
-}
-impl Default for ExtY_ASM_preshapeBesselF_T {
-    fn default() -> Self {
-        ExtY_ASM_preshapeBesselF_T {
-            cmd_f_ddot: 0f64,
-            cmd_f_dot: 0f64,
-            cmd_f: 0f64,
-        }
-    }
-}
-impl Default for DW_ASM_preshapeBesselF_T {
-    fn default() -> Self {
-        DW_ASM_preshapeBesselF_T {
-            SSflag_d_DSTATE: [0f64; 4],
-        }
-    }
-}
-impl AsmPreshapeFilter {
-    /// Creates a new controller
-    pub fn new() -> Self {
-        let mut this: Self = Default::default();
-        let mut data: RT_MODEL_ASM_preshapeBesselF_T = tag_RTM_ASM_preshapeBesselF_T {
-            dwork: &mut this.states as *mut _,
-        };
-        unsafe {
-            ASM_preshapeBesselF_initialize(
-                &mut data as *mut _,
-                &mut this.inputs as *mut _,
-                &mut this.outputs as *mut _,
-            )
-        }
-        this
-    }
-    /// Steps the controller
-    pub fn step(&mut self) {
-        let mut data: RT_MODEL_ASM_preshapeBesselF_T = tag_RTM_ASM_preshapeBesselF_T {
-            dwork: &mut self.states as *mut _,
-        };
-        unsafe {
-            ASM_preshapeBesselF_step(
-                &mut data as *mut _,
-                &mut self.inputs as *mut _,
-                &mut self.outputs as *mut _,
-            )
-        }
-    }
-}
+simulink_binder::import! {ASM_preshapeBesselF}
+pub type AsmPreshapeFilter = ASM_preshapeBesselF;
 
 #[cfg(test)]
 mod tests {
