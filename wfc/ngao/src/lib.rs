fe154e357a18171dbe8c40507c26e8ffb6875641
Rod Conan <rconan@gmto.org>
2023-03-20 11:39:28 -0700

    NGAO IMPULSE TEST

--- wfc/ngao/src/lib.rs
+++ wfc/ngao/src/lib.rs
@@ -6,3 +6,41 @@
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 include!(concat!(env!("OUT_DIR"), "/controller.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use matio_rs::MatFile;
+
+    #[test]
+    fn impulse() {
+        let path = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("..")
+            .join("simulink_models")
+            .join("ao_tests.mat");
+        let mat = MatFile::load(path.to_str().unwrap()).unwrap();
+        let ngao_imp_t: Vec<f64> = mat.var("Cngao_imp_t").unwrap();
+        let ngao_imp_y: Vec<f64> = mat.var("Cngao_imp_y").unwrap();
+
+        let n = ngao_imp_t.len();
+
+        let mut ctrl = NgaoTemporalCtrl::new();
+        let mut y = vec![];
+        for i in 0..n {
+            ctrl.inputs.Delta_m = if i == 0 { 4000f64 } else { 0f64 };
+            ctrl.step();
+            y.push(ctrl.outputs.m);
+        }
+        dbg!(&y[..10]);
+
+        let y_err = (ngao_imp_y
+            .into_iter()
+            .zip(&y)
+            .map(|(sim_y, y)| sim_y - *y)
+            .map(|x| x * x)
+            .sum::<f64>()
+            / n as f64)
+            .sqrt();
+        assert!(dbg!(y_err) < 1e-6);
+    }
+}
